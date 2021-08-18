e48662d53aa5fae6dd9b68d5d7c6f028b198b481
phelpa <pliniohelpa@gmail.com>
2021-08-18 08:55:15 -0300

    feat: add types in config

--- config.d.ts
+++ config.d.ts
@@ -1,5 +1,20 @@
-interface IMyFormik {
+import { FormikConfig, FormikProps } from 'formik'
+import { TextFieldProps } from '@material-ui/core'
+
+interface IUseFormParams<Values> extends FormikConfig<Values> {
+  removeEmptyValues?: boolean
 }
+
+export function useMyFormik<T>(config: IUseFormParams<T>): FormikProps<T> 
+
+interface IFormProps {
+  context: FormikProps<any>
+  children?: React.ReactNode
+}
+
+export const MyForm: React.NamedExoticComponent<IFormProps>
+
+export const MyFormikField: React.FC<TextFieldProps>
 
 
 
