b27cc52328bb6380441030f7975decfb196e7248
phelpa <pliniohelpa@gmail.com>
2021-08-18 08:34:03 -0300

    feat: add removeEmpty prop to the hook

---
+++ src/useMyFormik/index.ts
@@ -0,0 +1,33 @@
+import { useFormik, FormikConfig, FormikProps } from 'formik'
+import { useEffect, useCallback } from 'react'
+
+export interface IUseFormParams<Values> extends FormikConfig<Values> {
+  removeEmptyValues?: boolean
+}
+
+const removeEmptyPropertiesFromObject = (obj: any) => {
+  Object.keys(obj).forEach((k) => (!obj[k] && obj[k] !== undefined) && delete obj[k])
+  return obj
+}
+
+function useMyFormik<T>(config: IUseFormParams<T>): FormikProps<T> {
+
+  const formik = useFormik(config)
+
+  const { setValues, values } = formik
+
+  const formValues = useCallback((values) => {
+    const cleanObj = removeEmptyPropertiesFromObject(values)
+    return cleanObj
+  }, [])
+
+  useEffect(() => {
+    if(config.removeEmptyValues) {
+      setValues(formValues, false)
+    }
+  }, [setValues, formValues, values, config.removeEmptyValues])
+
+  return formik
+}
+
+export default useMyFormik
